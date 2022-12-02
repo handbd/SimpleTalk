24e26b60bd8c0e2db1d755c79391dd94fffd85d7
handbrennan <handbd@mail.uc.edu>
2022-12-02 14:51:46 -0500

    Initial commit

---
+++ src/main/java/com/example/SimpleTalk/SimpleTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimpleTalk/SimpleTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
